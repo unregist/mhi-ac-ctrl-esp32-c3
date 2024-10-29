a5b1a9b7dbfae5d380fc30007a4355969c1b604b
unregist <22579156+unregist@users.noreply.github.com>
2024-10-29 13:09:41 +1000

    Update MHI-AC-Ctrl-core.h

--- esphome/components/MhiAcCtrl/MHI-AC-Ctrl-core.h
+++ esphome/components/MhiAcCtrl/MHI-AC-Ctrl-core.h
@@ -5,12 +5,12 @@
 
 // # Config
 // ## pin defintions
-#define GPIO_MOSI                   GPIO_NUM_17 //Should be 7  on C3, Changing to 17 for ESP32 (non-C3)
-#define GPIO_MISO                   GPIO_NUM_16 //Should be 2  on C3, Changing to 16 for ESP32 (non-C3)
-#define GPIO_SCLK                   GPIO_NUM_18 //Should be 6  on C3, Changing to 18 for ESP32 (non-C3)
-#define GPIO_CS_OUT                 GPIO_NUM_25 //Should be 9  on C3, Changing to 25 for ESP32 (non-C3)
-#define GPIO_CS_IN                  GPIO_NUM_26 //Should be 10 on C3, Changing to 26 for ESP32 (non-C3)
-#define GPIO_IDK                    GPIO_NUM_21 // This was 3  on C3, I don't know if it does anything
+#define GPIO_MOSI                   GPIO_NUM_7
+#define GPIO_MISO                   GPIO_NUM_2
+#define GPIO_SCLK                   GPIO_NUM_6
+#define GPIO_CS_OUT                 GPIO_NUM_9
+#define GPIO_CS_IN                  GPIO_NUM_8 // GPIO_NUM_10 was used, however for my board 8 is easier
+#define GPIO_IDK                    GPIO_NUM_3 // GPIO_NUM_3 was used a few times but not connected, I don't know if it does anything
 
 #define RCV_HOST                    SPI2_HOST
 
